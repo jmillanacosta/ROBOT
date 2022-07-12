2f68e69e015243a04bde08ef5db1533f27e4c807
Javier <javier.millanacosta@maastrichtuniversity.nl>
2022-07-12 13:08:56 +0200

    changed variable name

--- eco_dump.sh
+++ eco_dump.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Retrieve ECO ontology (obo and owl)
 wget -N --no-if-modified-since https://raw.githubusercontent.com/evidenceontology/evidenceontology/master/eco.obo https://raw.githubusercontent.com/evidenceontology/evidenceontology/master/eco.owl
-#robot query --input eco.owl --query query.sparql result.csv
+robot query --input eco.owl --query query.sparql result.csv
